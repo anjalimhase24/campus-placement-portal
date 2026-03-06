0db4752c08b99c4d8ccf855dbbfaa571e28ed462
Darshana8916 <darshanadeore14@gmail.com>
2026-03-06 12:03:51 +0530

    add js

--- js/login.js
+++ js/login.js
@@ -1,6 +1,10 @@
 function toggleMenu() {
     document.querySelector(".navbar").classList.toggle("show");
 }
+
+
+
+
 function togglePassword(id) {
     const input = document.getElementById(id);
     input.type = input.type === "password" ? "text" : "password";
