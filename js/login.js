637758cffcea086f3bee1c848d34b9c16c6efb3c
Darshana8916 <darshanadeore14@gmail.com>
2026-03-06 09:44:29 +0530

    add info

--- js/login.js
+++ js/login.js
@@ -1,8 +1,8 @@
-    function toggleMenu() {
-      document.querySelector(".navbar").classList.toggle("show");
-    }
-    function togglePassword(id) {
-      const input = document.getElementById(id);
-      input.type = input.type === "password" ? "text" : "password";
-    }
-  
+function toggleMenu() {
+    document.querySelector(".navbar").classList.toggle("show");
+}
+function togglePassword(id) {
+    const input = document.getElementById(id);
+    input.type = input.type === "password" ? "text" : "password";
+}
+
